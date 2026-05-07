84630056f051359c68b58173b943c9e93e61d121
KenjiKagawa <kagawa6kenji@gmail.com>
2026-05-07 11:38:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+function startWizard() {
 
+  let spend = prompt("Spend level? (F2P / Low / Whale)");
+  let playstyle = prompt("Playstyle? (PvP / PvE / Balanced)");
+
+  let result = getStrategy(spend, playstyle);
+
+  document.getElementById("result").innerText = result;
+}
