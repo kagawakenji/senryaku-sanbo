1223c3cddfa069dd02322c9ce433ef9e4dca79c1
KenjiKagawa <kagawa6kenji@gmail.com>
2026-05-07 11:37:49 -0700

    Add getStrategy function for player strategy

--- rules.js
+++ rules.js
@@ -1,1 +1,12 @@
+function getStrategy(spend, playstyle) {
 
+  if (spend === "F2P" && playstyle === "PvP") {
+    return "F2P Raider: Focus economy + cheap troops + timing attacks";
+  }
+
+  if (spend === "Whale") {
+    return "Whale Commander: Focus fast growth + heavy PvP";
+  }
+
+  return "Balanced Player: Focus steady progression";
+}
